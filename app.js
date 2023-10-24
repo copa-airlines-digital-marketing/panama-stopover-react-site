4dc086eade6741ab687834557ae73d52d52f1457
cpatinoCopaair <cesar.arturo.patino@gmail.com>
2023-10-24 14:25:47 -0500

    fixed server routing issue

--- app.js
+++ app.js
@@ -4,8 +4,8 @@ const app = express();
 
 app.use(express.static(path.join(__dirname, 'build')));
 
-app.get('/', function (req, res) {
-  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+app.get('*', function(req, res) {
+  res.sendFile('index.html', {root: path.join(__dirname, 'build')});
 });
 
 app.listen(process.env.PORT || 5000);
